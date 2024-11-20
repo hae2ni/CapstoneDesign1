e7f27f7c1ac9c124f5f5124de557b70c861da8b4
HAE2NI <hae0_02ni@ewhain.net>
2024-11-20 15:55:01 +0900

    feat: header svg

--- photobook/src/components/Header.tsx
+++ photobook/src/components/Header.tsx
@@ -1,10 +1,25 @@
-import { ReactComponent as ClipIcon } from "../assets/ClipIt.svg";
+import ClipIcon from "../assets/ClipIt.svg?react";
+import { rowFlex } from "../styles/common";
 import NavBar from "./NavBar";
+import styled from "styled-components";
 
 export default function Header() {
   return (
-    <>
+    <Container>
+      <Icon />
       <NavBar />
-    </>
+    </Container>
   );
 }
+
+const Container = styled.header`
+  ${rowFlex}
+  justify-content: space-between;
+  width: 130.5rem;
+  height: 3.8rem;
+`;
+
+const Icon = styled(ClipIcon)`
+  width: 7.2rem;
+  height: 3.2rem;
+`;
